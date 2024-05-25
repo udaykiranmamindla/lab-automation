e3730d3cc10459ee6c5a27cebb9954df05e5df2c
udaykiran mamindla <129831999+udaykiranmamindla@users.noreply.github.com>
2024-05-25 14:29:28 +0530

    Add files via upload

---
+++ current_sensor_v2/constants.h
@@ -0,0 +1,16 @@
+#define THING_SPEAK_ADDRESS "api.thingspeak.com"
+
+#define MAIN_SSID "HITAM_S18"
+#define MAIN_PASS "hitam@S18"
+
+#define INTERVAL 15000
+#define CSE_IP "esw-onem2m.iiit.ac.in"
+#define CSE_PORT 443
+#define OM2M_ORGIN "kuku@@!123:KaAa@45"
+#define OM2M_MN "/~/in-cse/in-name/"
+#define OM2M_AE "HITAM_1"         // Change this
+#define OM2M_DATA_CONT "Node-1/Data"
+#define HTTPS false
+
+#define channel  2071415 // Change this
+#define writekey "DKMNLEXUQ85LUEQ6" // Change this
